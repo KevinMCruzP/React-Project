325396faeaa5add0aff42229ea6f35f4bed37952
Kevin Cruz <kevin_mcp@outlook.com>
2021-03-08 19:34:28 -0300

    import css files-SASS config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import './styles/global.scss'
+
 export function App() {
 
     return (
         <div>
-            <h1>Hello React</h1>
+            <h1>Hello World</h1>
             <h2>asd</h2>
         </div>
     )
