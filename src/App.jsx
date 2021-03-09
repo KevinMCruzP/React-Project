eadccdc2e2a2ac91a0c688cc96b1ca75ecdfd402
Kevin Cruz <kevin_mcp@outlook.com>
2021-03-09 13:53:09 -0300

    Fast Refresh config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Counter } from './components/Counter'
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
 
     return (
-        <div>
-            <h1>Hello World</h1>
-            <h2>asd</h2>
-        </div>
+        <>
+        <RepositoryList />
+        <Counter />
+        </>
     )
 
 
